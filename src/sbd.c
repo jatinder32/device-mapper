9a4714793f591044528f13dc6cb3a844f2deb3bb
jatinder <jatinder@jatinder.(none)>
2016-03-08 13:31:36 +0530

    [device-mapper]
    blok driver register

--- src/sbd.c
+++ src/sbd.c
@@ -1,21 +1,39 @@
 #include "../include/sbd_common.h"
 
 
-static int __init sbd_init(void)
+static int __init SBD_init(void)
 {
-	printk(KERN_INFO "Hello world\n");
-	myfunction();
+	int ret;
+	ENTRY;
+	sbd = (struct SBD*)kmalloc(sizeof(struct SBD),GFP_KERNEL);
+	if(sbd == NULL){
+		printk(KERN_ERR "malloc failure at %s \n", __func__);
+		goto OUT;
+	}
+
+	memset(sbd,'\0',sizeof(struct SBD));
+	
+	major_no = register_blkdev(0, DEVICE_NAME);
+	if(major_no < 0){
+		printk(KERN_ERR "Can't get major number\n");
+		goto OUT;
+	}
+	printk(KERN_INFO "Major Number = %d\n",major_no);
+	EXIT;
 	return 0;
 
+OUT:
+	printk(KERN_ERR "EXIT with error %s\n",__func__);
 }
 
-static void __exit sbd_exit(void)
+static void __exit SBD_exit(void)
 {
-	printk(KERN_INFO "my exit\n");
-
+	ENTRY;
+	unregister_blkdev(major_no, DEVICE_NAME);
+	EXIT;
 }
 
-module_init(sbd_init);
-module_exit(sbd_exit);
+module_init(SBD_init);
+module_exit(SBD_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("jatinder");
